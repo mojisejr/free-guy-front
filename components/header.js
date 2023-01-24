9ed72ea4b4922e68d01c40197a52768575fa8e79
nonthask laoluerat <nonthasak@Macintosh.local>
2023-01-24 16:29:25 +0700

    update motion

--- components/header.js
+++ components/header.js
@@ -1,15 +1,24 @@
 import Connectwallet from "./connectwallet";
 import styles from "../styles/header.module.css";
+import { motion } from "framer-motion";
 
 export default function Header() {
   return (
     <div className={styles.container}>
-      <div className={styles.logoWrapper}>
+      <motion.div
+        animate={{ x: [null, 20, 0] }}
+        transition={{ ease: "easeOut", duration: 1 }}
+        className={styles.logoWrapper}
+      >
         <img src="logo.png" width="" className={styles.logo}></img>
-      </div>
-      <div className={styles.connectwallet}>
+      </motion.div>
+      <motion.div
+        animate={{ x: [null, -20, 0] }}
+        transition={{ ease: "easeOut", duration: 1 }}
+        className={styles.connectwallet}
+      >
         <Connectwallet />
-      </div>
+      </motion.div>
     </div>
   );
 }
---
+++ pages/minting.js
@@ -0,0 +1,11 @@
+import { motion } from "framer-motion";
+
+export default function MintingPage() {
+  return (
+    <div>
+      <motion.div animate={{ x: 100 }}>
+        <div>test</div>
+      </motion.div>
+    </div>
+  );
+}
